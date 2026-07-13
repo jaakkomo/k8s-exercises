7b29f7febeb7233529c448691c16666b71df9dd4
jaakkomo <31561277+fallmx@users.noreply.github.com>
2026-07-13 20:38:03 +0300

    Create greeter

---
+++ greeter/greeter.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func readEnv(env, fallback string) string {
+	if value, ok := os.LookupEnv(env); ok {
+		return value
+	} else {
+		return fallback
+	}
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello from version 1")
+	})
+
+	port := readEnv("PORT", "8080")
+
+	fmt.Println("Server started in port", port)
+	http.ListenAndServe(":"+port, nil)
+}
